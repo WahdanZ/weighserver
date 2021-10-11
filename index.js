6013bf450a2c7d8627abdef731342d529465acc5
Ahmed Wahdan <ahmed.wahdan@outlook.com>
2021-10-11 23:17:12 +0200

    update

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 require("./routes/weigh_route")(app);
 
 // set port, listen for requests
-const PORT = 80;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}. db ${db.url}`);
 });
