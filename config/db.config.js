e4709ce7403885e51cdc8831fb46b50778928121
Ahmed Wahdan <ahmed.wahdan@outlook.com>
2021-10-12 09:46:46 +0200

    update db url

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: process.env.PORT || "mongodb+srv://admin:IdSELayuQStHGHS9@cluster0.pdj3g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    url: process.env.DB_URL ||  "mongodb://localhost:27017/weight_db"
 };
